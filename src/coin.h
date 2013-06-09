f2680b95374766fa848cd9e5fe829758b366e451
eagleeye <eagleeye@cpe10.(none)>
2013-06-09 07:04:06 +0200

    initial commit.

---
+++ src/coin.h
@@ -0,0 +1,45 @@
+/*
+ * coin.h
+ *
+ *  Created on: Apr 21, 2013
+ *      Author: ceperez
+ */
+
+#ifndef COIN_H_
+#define COIN_H_
+
+#define COIN_NAME "VerityCoin"
+
+#define COIN_ADDRESS_START  0
+
+#define COIN_HASH_GEN_BLOCK "0x2ad2fd149210469fb1c86c11e0ff6515eb08e297944694bef5031200ee8b1924"
+
+
+#define COIN_SUBSIDY_HALFLIFE 840000
+#define COIN_COINS_PER_BLOCK 12
+
+#define COIN_DEFAULT_PORT 9737
+#define COIN_TEST_DEFAULT_PORT 19737
+
+#define COIN_RETARGET_TIME 3.5 * 24 * 60 * 60
+#define COIN_RETARGET_SPACING 2.5 * 60
+#define COIN_INITIAL_DIFF 20
+
+#define COIN_DEV_ADDR_LENGTH 320
+#define COIN_64 100000000
+
+#define COIN_MAX_MONEY 84000000
+
+#define COIN_SCRYPT_SCRATCHPAD_SIZE 256
+
+#define COIN_MERKEL_ROOT  "0x34b8917e97f6fa3e1981e74dc2d9c646548925f32c32fc1346a91d82ca7b8cdd"
+
+#define COIN_DNS_SEED {"node.zenitcoin.com", "seed.zenithcoin.com"},
+
+#define COIN_IRC_CHANNELS 1
+
+#define COIN_SEED  
+
+// 0x529a1347, 0x2d3de442,
+
+#endif /* COIN_H_ */
